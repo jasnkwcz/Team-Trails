6909f0d847b981d6768bcea746127dc748807f5f
Anthony Nice <nicea@oregonstate.edu>
2020-11-07 16:44:49 -0700

    Auto stash before merge of "main" and "origin/main"
    Directions button HTML and JavaScript file.  just test functionality at this point.

---
+++ directions/directions.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+
+// Test URL
+var url = "https://www.google.com/maps/dir/?api=1&origin=Space+Needle+Seattle+WA&destination=Pike+Place+Market+Seattle+WA"
+
+
+
+function getDirections(){
+	window.open(url, "_blank");
+}
+
