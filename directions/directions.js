25c0d529ad4b4d94b3576b8e4f630bcc6c13aceb
Anthony Nice <nicea@oregonstate.edu>
2020-11-08 10:35:51 -0700

    Auto stash before merge of "main" and "origin/main"
    Added the users location to directions.

--- directions/directions.js
+++ directions/directions.js
@@ -1,7 +1,11 @@
 // JavaScript source code
 
+// Get the users location.
+var city = localStorage.getItem("city");
+var state = localStorage.getItem("state");
+
 // Test URL
-var url = "https://www.google.com/maps/dir/?api=1&origin=Space+Needle+Seattle+WA&destination=Pike+Place+Market+Seattle+WA"
+var url = "https://www.google.com/maps/dir/?api=1&origin" + city + "+" + state + "&destination=Pike+Place+Market+Seattle+WA"
 
 
 
