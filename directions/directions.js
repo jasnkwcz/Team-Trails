6805b2786530e996be48fef206f7725e07166211
Anthony Nice <nicea@oregonstate.edu>
2020-11-08 19:53:06 -0700

    Added Lat and Long for the trailhead.  Waiting to implement lat and long from trail API.

--- directions/directions.js
+++ directions/directions.js
@@ -4,8 +4,13 @@
 var city = localStorage.getItem("city");
 var state = localStorage.getItem("state");
 
+// Get the Latitude and Longitude for the trailhead.
+// HARD CODED RIGHT NOW. WORKING ON GETTING LAT AND LONG FROM API.
+var lat = 39.9527;
+var long =  -105.2313;
+
 // Test URL
-var url = "https://www.google.com/maps/dir/?api=1&origin" + city + "+" + state + "&destination=Pike+Place+Market+Seattle+WA"
+var url = "https://www.google.com/maps/dir/?api=1&origin" + city + "+" + state + "&destination=" + lat + "," + long;
 
 
 
