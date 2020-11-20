4278cf23ea479c817db1f511837e0d5f30aa632b
ragnakevmac <59546462+ragnakevmac@users.noreply.github.com>
2020-11-20 05:00:03 -0800

    Add files via upload

--- trails/src/Components/Profile.js
+++ trails/src/Components/Profile.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
   Link
 } from "react-router-dom";
 
@@ -28,8 +25,91 @@ export default class Profile extends Component{
     }
     
     
-    calculateFitLevel(DA, age, kg, cm) {
-        let result = parseInt(DA)
+    // calculateFitLevel(DA, age, kg, cm) {
+    //     let result = parseInt(DA)
+    //     let agePts = 0
+    //     let BMIpts = 0
+    
+    //     if(age >= 6 && age <= 17){
+    //         agePts = 5
+    //         result += 5
+    //     } 
+    //     else if (age >= 18 && age <= 64) {
+    //         agePts = 3
+    //         result += 3
+    //     } 
+    //     else if (age >= 65) {
+    //         agePts = 1
+    //         result += 1
+    //     } 
+    //     else {
+    //         agePts = 0
+    //         result = 0
+    //     }
+    
+    //     let BMI = kg / ((cm/100)**2)
+    
+    //     if (BMI >= 18.5 && BMI <= 24.9) {
+    //         BMIpts = 2
+    //         result += 2
+    //     }
+    
+    //     let sum = result
+    
+    //     result = Math.round(result * 5 / 12);
+    
+    //     document.getElementById("showWork1").textContent = `${Math.round(BMI*10)/10} = ${kg} / (${cm}/100)^2`
+    //     document.getElementById("showWork2").textContent = `${sum} = ${DA} + ${agePts} + ${BMIpts}`
+    //     document.getElementById("showWork3").textContent = `${result} = round(${sum} * 5 / 12)`
+    
+    //     return result;
+    // }
+    
+
+    componentDidMount() {
+      this.props.calculateFitness();
+    }
+    
+    render(){
+      
+      let name = this.props.userState.name;
+      let gender = this.props.userState.gender;
+      let age = this.props.userState.age;
+      let city = this.props.userState.city;
+      let state = this.props.userState.state;
+      let zip = this.props.userState.zip;
+      let height = this.props.userState.height;
+      let weight = this.props.userState.weight;
+
+      let fitness = this.props.userState.fitnessLevel
+
+      let activities = [
+        "Not Active",
+        "Slightly Active",
+        "Moderately Active",
+        "Very Active",
+        "A Marathon Runner Level!"
+      ];
+
+      let activityNum = this.props.userState.activity;
+      let activity = activities[activityNum - 1];
+
+
+
+      let recommendations = [
+        "the easiest",
+        "some easy",
+        "some intermediate level",
+        "some challenging",
+        "the most challenging"
+      ];
+
+      let recommendation = recommendations[fitness - 1];
+
+
+      function localCalc(activityNum, age, weight, height) {
+
+        let result = parseInt(activityNum)
         let agePts = 0
         let BMIpts = 0
     
@@ -50,7 +130,7 @@ export default class Profile extends Component{
             result = 0
         }
     
-        let BMI = kg / ((cm/100)**2)
+        let BMI = weight / ((height/100)**2)
     
         if (BMI >= 18.5 && BMI <= 24.9) {
             BMIpts = 2
@@ -58,35 +138,38 @@ export default class Profile extends Component{
         }
     
         let sum = result
-    
-        result = Math.round(result * 5 / 12);
-    
-        document.getElementById("showWork1").textContent = `${Math.round(BMI*10)/10} = ${kg} / (${cm}/100)^2`
-        document.getElementById("showWork2").textContent = `${sum} = ${DA} + ${agePts} + ${BMIpts}`
-        document.getElementById("showWork3").textContent = `${result} = round(${sum} * 5 / 12)`
-    
-        return result;
-    }
-    
-    
-    render(){
+
+        return [sum, activityNum, agePts, BMIpts];
+
+      }
+
+      let calcArr = localCalc(activityNum, age, weight, height);
+
+      let sum = calcArr[0];
+      let DA = calcArr[1];
+      let agePts = calcArr[2];
+      let BMIpts = calcArr[3];
+
+
+
       return (
-        <div>
+        <div class="profilePages">
           <h2>My Profile</h2>
-      Name: <span id="showName">{this.props.userState.name}</span><br />
-          Gender: <span id="showGender">{this.props.userState.gender}</span><br />
-          Age: <span id="showAge">{this.props.userState.age}</span><br />
-          City: <span id="showCity">{this.props.userState.city}</span><br />
-          State: <span id="showState">{this.props.userState.state}</span><br />
-          Zip Code: <span id="showZip">{this.props.userState.zip}</span><br />
-          Height: <span id="showHeight">{this.props.userState.height}</span>cm<br />
-          Weight: <span id="showWeight">{this.props.userState.weight}</span>kg<br />
-          Daily Activity: <span className="showActivity">{this.props.userState.activity}</span><br />
-          <p style={{color: 'green', fontWeight: 'bold'}}>Fitness Level: <span id="showActivityNum">{this.props.userState.fitnessLevel}</span></p>
+          Name: <span id="showName">{name}</span><br />
+          Gender: <span id="showGender">{gender}</span><br />
+          Age: <span id="showAge">{age}</span><br />
+          City: <span id="showCity">{city}</span><br />
+          State: <span id="showState">{state}</span><br />
+          Zip Code: <span id="showZip">{zip}</span><br />
+          Height: <span id="showHeight">{height}</span>cm<br />
+          Weight: <span id="showWeight">{weight}</span>kg<br />
+          Daily Activity: <span className="showActivity">{activity}</span><br />
+          <p style={{color: 'green', fontWeight: 'bold'}}>Fitness Level: <span id="showActivityNum">{fitness}</span></p>
           <br />
           <p>
-            Your Fitness Level has been calculated to <span className="level" style={{color: 'green'}}>{this.props.userState.recommendedTrailLevel}</span>.. Here's the formula:<br />
-          </p><div>
+      Your Fitness Level has been calculated to <span className="level" style={{color: 'green'}}>{fitness}</span>. Here's the formula:<br />
+          </p>
+          <div class="points">
             <strong>Daily Activity:</strong><br /><table>
               <tbody><tr />
                 <tr> <th>Not Active</th>            <td>+1 point</td>  </tr>
@@ -96,7 +179,7 @@ export default class Profile extends Component{
                 <tr> <th>A Marathon Runner!</th>    <td>+5 points</td> </tr>
               </tbody></table>
           </div>
-          <div>
+          <div class="points">
             <strong>Age:</strong><br /><table>
               <tbody><tr />
                 <tr> <th>6 ~ 17</th>  <td>+5 points</td> </tr>
@@ -105,7 +188,7 @@ export default class Profile extends Component{
                 <tr> <th>Other</th> <td>+0 points</td> </tr>
               </tbody></table>
           </div>
-          <div>
+          <div class="points">
             <strong>BMI (kg/m^2):</strong><br /><table>
               <tbody><tr />
                 <tr> <th>18.5 ~ 24.9</th>  <td>+2 points</td> </tr>
@@ -120,12 +203,18 @@ export default class Profile extends Component{
             Fitness Level = round(Sum * 5 / 12)
           </p>
           <p style={{color: 'green'}}>
-            <span id="showWork1" /><br />
-            <span id="showWork2" /><br />
-            <span id="showWork3" />
+            <span id="showWork1">
+              {Math.round(this.props.userState.weight/((height/100)**2)*10)/10} = {weight} / ({height}/100)^2
+            </span><br />
+            <span id="showWork2">
+              {sum} = {DA} + {agePts} + {BMIpts}
+            </span><br />
+            <span id="showWork3">
+              {fitness} = round({sum} * 5 / 12)
+            </span><br />
           </p>
           <br />
-          <p>This means that we will recommend you <span id="recommend" style={{color: 'green'}} /> trails available.</p>
+          <p>This means that we will recommend you <span id="recommend" style={{color: 'green'}}>{recommendation}</span> trails available.</p>
           <p>
             But as your Fitness Level changes, your recommentations will also change. <br />
             It may increase as you become more active and have the right weight, <br />
@@ -137,7 +226,7 @@ export default class Profile extends Component{
           <br />
           <button id="delete" onClick={this.delClick}>
             
-          <Link id="deleteButton" className="navitem" to="/profile/edit">Delete Profile</Link>
+          <Link  id="deleteButton" className="navitem" to="/profile/edit">Delete Profile</Link>
           
           </button>
           <br/ >
