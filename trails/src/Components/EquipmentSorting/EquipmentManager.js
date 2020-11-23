38cc660263d382937d4b2434c83fd54ba825a35a
gjcooper99 <gjcooper99@gmail.com>
2020-11-22 16:35:17 -0800

    Rearranged some minor code

--- trails/src/Components/EquipmentSorting/EquipmentManager.js
+++ trails/src/Components/EquipmentSorting/EquipmentManager.js
@@ -6,12 +6,7 @@ function EquipmentManager() {
     return (
         <div className="wrapper">
       {data.map(equipment => (
-        <Equipment 
-        key={equipment.name}
-        name={equipment.name}
-        image={equipment.image}
-        description={equipment.description}
-        />
+        <Equipment key={equipment.name} name={equipment.name} image={equipment.image} description={equipment.description} />
       ))}
         </div>
     )
