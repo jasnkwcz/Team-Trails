0866a9791317f712e2e8e062a0301bc7bf439b08
ragnakevmac <59546462+ragnakevmac@users.noreply.github.com>
2020-12-02 10:32:10 -0800

    User Profile changes
    Pre-populate the text box when editing info with previous values.

--- trails/src/Components/UserForm.js
+++ trails/src/Components/UserForm.js
@@ -39,28 +39,39 @@ export default class UserForm extends Component{
 
 
     render(){
+
+      let nameEdit = this.props.userState.name;
+      let genderEdit = this.props.userState.gender;
+      let ageEdit = this.props.userState.age;
+      let cityEdit = this.props.userState.city;
+      let stateEdit = this.props.userState.state;
+      let zipEdit = this.props.userState.zip;
+      let heightEdit = this.props.userState.height;
+      let weightEdit = this.props.userState.weight;
+
+
       return (
         <div class="profilePages">
           <h2>Create Profile</h2>
           <h3>And calculate Your Fitness Level:</h3> 
           <form>
-            Name: <input onChange={this.updateInfo} type="text" id="inputName" name="name" placeholder="e.g. Kevin (optional)" />
+            Name: <input onChange={this.updateInfo} type="text" id="inputName" name="name" placeholder="e.g. Kevin (optional)" value={nameEdit} />
             <br />
             Gender: 
-            <input onChange={this.updateInfo} type="radio" name="gender" defaultValue="Male" />Male
-            <input onChange={this.updateInfo} type="radio" name="gender" defaultValue="Female" />Female
+            <input onChange={this.updateInfo} type="radio" name="gender" Value="Male"  />Male
+            <input onChange={this.updateInfo} type="radio" name="gender" Value="Female"  />Female
             <br />
-            Age: <input onChange={this.updateInfo} type="number" id="inputAge" name="age" placeholder="e.g. 29" />
+            Age: <input onChange={this.updateInfo} type="number" id="inputAge" name="age" placeholder="e.g. 29" value={ageEdit} />
             <br />
-            City Name: <input onChange={this.updateInfo} type="text" id="inputCity" name="city" placeholder="e.g. Corvallis" />
+            City Name: <input onChange={this.updateInfo} type="text" id="inputCity" name="city" placeholder="e.g. Corvallis" value={cityEdit} />
             <br />
-            State (e.g. "OR"): <input onChange={this.updateInfo} type="text" id="inputState" name="state" placeholder="e.g. OR" />
+            State (e.g. "OR"): <input onChange={this.updateInfo} type="text" id="inputState" name="state" placeholder="e.g. OR" value={stateEdit} />
             <br />
-            Zip Code: <input onChange={this.updateInfo} type="number" id="inputZip" name="zip" placeholder="e.g. 97333" />
+            Zip Code: <input onChange={this.updateInfo} type="number" id="inputZip" name="zip" placeholder="e.g. 97333" value={zipEdit} />
             <br />
-            Height(in cm): <input onChange={this.updateInfo} type="number" id="inputHeight" name="height" placeholder="e.g. 160" />
+            Height(in cm): <input onChange={this.updateInfo} type="number" id="inputHeight" name="height" placeholder="e.g. 160" value={heightEdit} />
             <br />
-            Weight(in kg): <input onChange={this.updateInfo} type="number" id="inputWeight" name="weight" placeholder="e.g. 60.2" />
+            Weight(in kg): <input onChange={this.updateInfo} type="number" id="inputWeight" name="weight" placeholder="e.g. 60.2" value={weightEdit} />
             <br />
             Daily Activity: 
             <select onChange={this.updateInfo} name="activity" id="activity">
