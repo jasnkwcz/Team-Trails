b4aa02522ca0509ed49058011fc971cc61c5b19a
ragnakevmac <59546462+ragnakevmac@users.noreply.github.com>
2020-12-03 05:45:16 -0800

    Revert "User Profile Style Change"
    This reverts commit 870f8b7171ba85fc735ca771525f2ea0f8c0e68b.

--- trails/src/Components/Profile.js
+++ trails/src/Components/Profile.js
@@ -1,4 +1,3 @@
-import { red } from "@material-ui/core/colors";
 import React, { Component } from "react";
 import {
   Link
@@ -179,14 +178,14 @@ export default class Profile extends Component{
             So make sure to keep updating your profile info by clicking "Edit Profile" below! <br />
             Otherwise, click the "Nearby Trails" tab on the top right of the page to see some trails!
           </p>
-          <Link className="editprofile" exact to="/profile/edit" style={{color: "blue", fontWeight: "bold"}}>Edit Profile</Link>
+          <Link className="editprofile" exact to="/profile/edit">Edit profile</Link>
           <br />
           <br />
-          <span id="delete" onClick={this.delClick} >
+          <button id="delete" onClick={this.delClick}>
             
-            <Link  id="deleteButton" className="navitem" to="/profile/edit" style={{color: "red", textDecoration: "underline"}}>Delete Profile</Link>
+            <Link  id="deleteButton" className="navitem" to="/profile/edit">Delete Profile</Link>
           
-          </span>
+          </button>
           <br/ >
           <br/ >
           <Link className="home" exact to="/">Back to First Page</Link>
